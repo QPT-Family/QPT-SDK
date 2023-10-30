b12c58dc222e329cb2aab9c044b94354f7dcf8c8
GT-Zhang <1029550448@qq.com>
2023-10-30 23:59:16 +0800

    喜提Bug

--- QEnvPython/38/setup.py
+++ QEnvPython/38/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='QEnvPython',
-    version="3.8.10.2",
+    version="3.8.10.3",
     packages=["QPT_SDK"],
     url='https://github.com/GT-ZhangAcer/QPT',
     license='MIT',
