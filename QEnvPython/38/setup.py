0a90c29bd5cbf57a74117aa413274bf5b65aa619
GT-Zhang <1029550448@qq.com>
2023-10-30 23:36:30 +0800

    喜提Bug

--- QEnvPython/38/setup.py
+++ QEnvPython/38/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup(
     name='QEnvPython',
     version="3.8.10.2",
-    packages=["python", "tkinter"],
+    packages=["QPT_SDK"],
     url='https://github.com/GT-ZhangAcer/QPT',
     license='MIT',
     author='GT-ZhangAcer',
@@ -16,5 +16,5 @@ setup(
     description='QPT-Python环境打包工具 SDK',
     python_requires='>=3.8',
     include_package_data=True,
-    include_dirs=["python", "tkinter"]
+    include_dirs=["QPT_SDK"]
 )
--- QVCRedist/setup.py
+++ QVCRedist/setup.py
@@ -7,8 +7,8 @@ from setuptools import setup
 
 setup(
     name='QVCRedist',
-    version="1.0",
-    packages=["vcredist"],
+    version="1.0.1",
+    packages=["QPT_SDK"],
     url='https://github.com/GT-ZhangAcer/QPT',
     license='MIT',
     author='GT-ZhangAcer',
@@ -16,5 +16,5 @@ setup(
     description='QPT-Python环境打包工具 SDK',
     python_requires='>=3.8',
     include_package_data=True,
-    include_dirs=["vcredist"]
+    include_dirs=["QPT_SDK"]
 )
