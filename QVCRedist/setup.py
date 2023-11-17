365e113492170304e88dfd6d3c08f71df0a6dad5
GT-Zhang <1029550448@qq.com>
2023-11-18 00:38:11 +0800

    增加一层目录

--- QVCRedist/setup.py
+++ QVCRedist/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='QVCRedist',
-    version="1.0.1",
+    version="1.0.2",
     packages=["QPT_SDK"],
     url='https://github.com/GT-ZhangAcer/QPT',
     license='MIT',
