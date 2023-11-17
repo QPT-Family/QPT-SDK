3c6098ee42e48ad7434ea3d79d51846762bbec4f
GT-Zhang <1029550448@qq.com>
2023-11-17 23:20:39 +0800

    空包上传

---
+++ setup.py
@@ -0,0 +1,20 @@
+# Author: Acer Zhang
+# Datetime: 2023/11/17
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='QPT_SDK',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/GT-ZhangAcer/QPT',
+    license='LGPL',
+    author='GT-ZhangAcer',
+    author_email='zhangacer@foxmail.com',
+    description='QPT_SDK占位文件夹'
+)
