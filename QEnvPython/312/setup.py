82d1f2f7e717ce6398758b0451208043de51a027
GT-Zhang <1029550448@qq.com>
2023-11-26 00:11:20 +0800

    增加10-12版本

---
+++ QEnvPython/312/setup.py
@@ -0,0 +1,20 @@
+# Author: Acer Zhang
+# Datetime: 2023/10/29
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+from setuptools import setup
+
+setup(
+    name='QEnvPython',
+    version="3.12.0",
+    packages=["QPT_SDK"],
+    url='https://github.com/GT-ZhangAcer/QPT',
+    license='MIT',
+    author='GT-ZhangAcer',
+    author_email='zhangacer@foxmail.com',
+    description='QPT-Python环境打包工具 Python SDK',
+    # python_requires='>=3.8',
+    include_package_data=True,
+    include_dirs=["QPT_SDK"]
+)
